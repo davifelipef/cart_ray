c86b01602ced1ba5239ce0f854d4e745ee0968c1
Davi Felipe <feliperedfield@gmail.com>
2024-06-05 12:05:42 -0300

    Basic layout created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   // Text fields setup variables
-  final TextEditingController _tickerController = TextEditingController();
-  final TextEditingController _quantityController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _typeController = TextEditingController();
+  final TextEditingController _valueController = TextEditingController();
 
   // Hive related setup 
   late Map<int, String> eventsMap = {};
@@ -38,34 +39,35 @@ class _HomePageState extends State<HomePage> {
   final primaryButton = Colors.black;
   final primaryBackground = Colors.white;
 
+  // App programming logic STARTS here
   @override
   void initState() {
     super.initState();
     // Date setup
-    currentDate = DateTime.now(); 
-    year = currentDate.year;
-    month = currentDate.month;
-    day = currentDate.day;
-    formattedDate = DateFormat('dd/MM/yyyy').format(currentDate);
+    currentDate = DateTime.now(); // Gets the current date
+    year = currentDate.year; // Gets the current date's year
+    month = currentDate.month; // Gets the current date's month
+    day = currentDate.day; // Gets the current date's day
+    formattedDate = DateFormat('dd/MM/yyyy').format(currentDate); // formats the date 
 
     print("Today's date is $formattedDate.");
     _refreshItems();
   }
 
+  // Updates the screen when a new event is added
   void _refreshItems() {
     final data = _eventsBox.keys.map((key) {
       final item = _eventsBox.get(key);
       return {
-        "key": key,
-        "ticker": item["ticker"],
-        "quantity": item["quantity"],
-        "dividend": item["dividend"],
-        "total": item["total"]
+        "key": key, // unique key of the event
+        "name": item["name"], // name of the event
+        "type": item["type"], // type of the event: money entry or exit
+        "value": item["value"], // value moved
       };
     }).toList();
 
-    // Sort the list based on the "ticker" field
-    data.sort((a, b) => (a["ticker"] as String).compareTo(b["ticker"] as String));
+    // Sort the list based on the "name" field
+    //data.sort((a, b) => (a["name"] as String).compareTo(b["name"] as String));
 
     setState(() {
       _events = data.toList();
@@ -73,9 +75,9 @@ class _HomePageState extends State<HomePage> {
     
   }
 
-  // Create new item
-  Future<void> _createItem(Map<String, dynamic> newItem) async {
-    await _eventsBox.add(newItem);
+  // Creates a new item
+  Future<void> _createItem(Map<String, dynamic> newEvent) async {
+    await _eventsBox.add(newEvent);
     _refreshItems(); // Updates the UI
   }
 
@@ -91,16 +93,16 @@ class _HomePageState extends State<HomePage> {
     print("$itemKey removed from the eventsMap");
     print("Current eventsMap: $eventsList"); 
 
-    // Get the ticker associated with the itemKey being deleted
+    // Get the name associated with the itemKey being deleted
     final item = _events.firstWhere((element) => element["key"] == itemKey);
-    final tickerToDelete = item["ticker"];
+    final nameToDelete = item["name"];
 
     await _eventsBox.delete(itemKey);
-    print("$itemKey removed from the tickerBox");  
-    print("Current tickerBox: $eventsList");   
+    print("$itemKey removed from the nameBox");  
+    print("Current nameBox: $eventsList");   
 
-    // Remove the ticker from eventsList
-    eventsList.remove(tickerToDelete);
+    // Remove the name from eventsList
+    eventsList.remove(nameToDelete);
     print("$itemKey removed from the eventsList");
     print("Current eventsList: $eventsList");    
 
@@ -118,18 +120,20 @@ class _HomePageState extends State<HomePage> {
     );
   }   
     
-  // Creates the dialog to add new tickers to the ticker list
+  // Creates the dialog to add new names to the name list
   void _showForm(BuildContext ctx, int? itemKey) async {
     
     if (itemKey != null) {
       final existingItem = 
       _events.firstWhere((element) => element["key"] == itemKey);
-      _tickerController.text = existingItem["ticker"];
-      _quantityController.text = existingItem["quantity"];
+      _nameController.text = existingItem["name"];
+      _typeController.text = existingItem["type"];
+      _valueController.text = existingItem["type"];
     } else {
       //Clear the text fields
-      _tickerController.text = "";
-      _quantityController.text = "";
+      _nameController.text = "";
+      _typeController.text = "";
+      _valueController.text = "";
     }
     
     showModalBottomSheet(
@@ -145,18 +149,24 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             children: <Widget>[
               TextField(
-                controller: _tickerController,
+                controller: _nameController,
                 decoration: const InputDecoration(
-                  hintText: "Código do Ticker"
+                  hintText: "Nome do registro"
                 ),
               ),
               const SizedBox(
                 height: 20,
               ),
               TextField(
-                controller: _quantityController,
+                controller: _typeController,
                 decoration: const InputDecoration(
-                  hintText: "Número de Cotas"
+                  hintText: "Tipo de registro"
+                ),
+              ),
+              TextField(
+                controller: _valueController,
+                decoration: const InputDecoration(
+                  hintText: "Valor"
                 ),
               ),
               const SizedBox(
@@ -170,19 +180,22 @@ class _HomePageState extends State<HomePage> {
                 onPressed: () async {
                   if (itemKey == null) {
                     _createItem({
-                      "ticker": _tickerController.text,
-                      "quantity": _quantityController.text,
+                      "name": _nameController.text,
+                      "type": _typeController.text,
+                      "value": _valueController.text,
                     });
                   }
                   if (itemKey != null) {
                     _updateItem(itemKey, {
-                      "ticker": _tickerController.text.trim(),
-                      "quantity": _quantityController.text.trim()
+                      "name": _nameController.text.trim(),
+                      "type": _typeController.text,
+                      "value": _valueController.text.trim()
                     });
                   }
                   //Clear the text fields
-                  _tickerController.text = "";
-                  _quantityController.text = "";
+                  _nameController.text = "";
+                  _typeController.text = "";
+                  _valueController.text = "";
                   // Closes the modal window
                   Navigator.of(context).pop(); 
                 }, 
@@ -194,7 +207,9 @@ class _HomePageState extends State<HomePage> {
         ),
         );
   }
+  // App programming logic ENDS here
 
+  // Build setup starts here
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -247,11 +262,11 @@ class _HomePageState extends State<HomePage> {
             itemCount: _events.length,
             itemBuilder: (_, index) {
               final currentItem = _events[index];
-              if (currentItem["dividend"] == null && eventsMap[currentItem["key"]] == null) {
+              if (currentItem["value"] == null && eventsMap[currentItem["key"]] == null) {
               }
-              final dividendValue = currentItem["dividend"] ?? eventsMap[currentItem["key"]] ?? "Carregando";
+              final dividendValue = currentItem["value"] ?? eventsMap[currentItem["key"]] ?? "Carregando";
               var sumDividend = double.tryParse(dividendValue.replaceAll(',', '.')) ?? 0.00;
-              var currentQuantity = double.tryParse(currentItem["quantity"]?.replaceAll(',', '.') ?? "0") ?? 0;
+              var currentQuantity = double.tryParse(currentItem["type"]?.replaceAll(',', '.') ?? "0") ?? 0;
               final dividendYield = currentItem["total"] ?? currentQuantity * sumDividend;
 
               try {
@@ -259,7 +274,7 @@ class _HomePageState extends State<HomePage> {
                   Map<String, dynamic>? existingItem = _eventsBox.get(currentItem["key"]);
                   if (existingItem != null) {
                     existingItem.addAll({
-                      "dividend": dividendValue,
+                      "value": dividendValue,
                       "total": dividendYield,
                     });
                     _eventsBox.put(currentItem["key"], existingItem);
@@ -279,7 +294,7 @@ class _HomePageState extends State<HomePage> {
                 elevation: 3,
                 child: ListTile(
                   title: Text(
-                    currentItem["ticker"] ?? "Erro ao retornar o ticker",
+                    currentItem["name"] ?? "Erro ao retornar o name",
                     style: const TextStyle(
                       fontSize: 15,
                       fontWeight: FontWeight.bold,
@@ -289,15 +304,11 @@ class _HomePageState extends State<HomePage> {
                     children: <Widget>[
                       Align(
                         alignment: Alignment.topLeft,
-                        child: Text("Cotas: ${currentItem["quantity"].toString()}"),
+                        child: Text("Tipo de evento: ${currentItem["type"].toString()}"),
                       ),
                       Align(
                         alignment: Alignment.topLeft,
-                        child: Text("Dividendo anunciado: R\$ ${sumDividend.toStringAsFixed(2)}"),
-                      ),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text("Total recebido: R\$ ${dividendYield.toStringAsFixed(2)}"),
+                        child: Text("Valor: R\$ ${sumDividend.toStringAsFixed(2)}"),
                       ),
                     ],
                   ),
@@ -338,9 +349,9 @@ class _HomePageState extends State<HomePage> {
   double calculateTotalReceivedDividends() {
     double total = 0.0;
     for (final item in _events) {
-      final dividendValue = item["dividend"] ?? eventsMap[item["key"]] ?? "0.00";
+      final dividendValue = item["value"] ?? eventsMap[item["key"]] ?? "0.00";
       final sumDividend = double.tryParse(dividendValue.replaceAll(',', '.')) ?? 0.00;
-      var currentQuantity = double.tryParse(item["quantity"]?.replaceAll(',', '.') ?? "0") ?? 0;
+      var currentQuantity = double.tryParse(item["type"]?.replaceAll(',', '.') ?? "0") ?? 0;
       final dividendYield = currentQuantity * sumDividend;
       total += dividendYield;
     }
