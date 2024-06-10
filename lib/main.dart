eb23255eb8e746585bcdccd91a6e3646d3e96c4f
Davi Felipe <feliperedfield@gmail.com>
2024-06-10 20:18:26 -0300

    Revert "Added localization to Brazilian Portuguese language"
    This reverts commit 6e79fe410c86deae330a4efbbf75f15da65dfce1.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:cart_ray/pages/home_page.dart';
 import 'package:cart_ray/utils/constants.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,13 +18,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('pt', 'BR'), // Brazilian Portuguese
-      ],
       debugShowCheckedModeBanner: false, //if set to false, disables the debug banner
       home: const HomePage(),
       theme: ThemeData(
